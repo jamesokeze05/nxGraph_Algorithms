edef1b3ce71ed3e761dca5ac7aaa648eb2d5bd3b
King James <49925762+jamesokeze05@users.noreply.github.com>
2019-04-25 16:06:17 -0500

    Update havel_hakimi.py

--- nxGraph_Algorithms/havel_hakimi.py
+++ nxGraph_Algorithms/havel_hakimi.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-
 import networkx as nx
-from Functions.global_properties import *
+from Functions.global_properties import degree_sequence
 
 
 def Havel_Hakimi_Derivative(D):
     for i in range(1, D[0] +1):
         D[i] -= 1
-        
-    D.pop(0)
-    D.sort(reverse = True)
-    return None
+        D.pop(0)
+        D.sort(reverse = True)
+    return D
 
 def Havel_Hakimi_Process(D):
     while D[0] > 0:
@@ -24,3 +21,7 @@ def residue(G):
     D = degree_sequence(G)
     Havel_Hakimi_Process(D)
     return len(D)
+
+G = nx.read_edgelist('Test_Graphs/G1.txt')
+nx.draw_networkx(G)
+print(Havel_Hakimi_Derivative(G))
